f009d10d6ce3227eeed555892654868d25ccdb0f
Greg Engelstad <greg03@pobox.com>
2021-09-17 16:14:06 -0700

    Add files via upload

--- Symulator3_VS_GUI/CodeBoxControl/Properties/AssemblyInfo.cs
+++ Symulator3_VS_GUI/CodeBoxControl/Properties/AssemblyInfo.cs
@@ -83,5 +83,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("0.1.2.21")]
+[assembly: AssemblyVersion ("0.1.2.22")]
 [assembly: AssemblyFileVersion ("0.1.0.24")]
